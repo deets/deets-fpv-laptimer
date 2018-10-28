cc66d26e318c492fc798ea67224c9df3a0fd7bea
Diez B. Roggisch <deets@web.de>
2018-10-28 19:15:31 +0100

    Now visualisation with channel-names

--- visualisation/main.py
+++ visualisation/main.py
@@ -7,8 +7,17 @@ import serial
 from bokeh.models import ColumnDataSource
 from bokeh.plotting import curdoc, figure
 from bokeh.layouts import column
+from bokeh.models import FactorRange
 
 from tornado import gen
+
+CHANNEL_NAMES = (
+    "E4", "R1", "E3", "E2", "R2", "E1", "A8", "R3",
+    "B1", "F1", "A7", "B2", "F2", "A6", "R4", "B3",
+    "F3", "A5", "B4", "F4", "A4", "R5", "B5", "F5",
+    "A3", "B6", "F6", "R6", "A2", "B7", "F7", "A1",
+    "B8", "F8", "R7", "E5", "E6", "R8", "E7", "E8",
+)
 
 
 def read_data(doc, source, port="/dev/ttyUSB0", baudrate=1000_000):
@@ -39,6 +48,7 @@ def main():
     # see the same document.
     doc = curdoc()
     data = dict(
+        channel_names=CHANNEL_NAMES,
         left=list(range(0, 80, 2)),
         right=list(range(1, 81, 2)),
         bottom=[0] * 40,
@@ -48,12 +58,16 @@ def main():
     source = ColumnDataSource(
             data=data,
     )
-    channel_figure = figure(width=600, height=400)
-    channel_figure.quad(
-        left="left",
-        right="right",
-        bottom="bottom",
-        top="readings",
+
+    channel_figure = figure(
+        width=600,
+        height=400,
+        x_range=FactorRange(*CHANNEL_NAMES),
+    )
+    channel_figure.vbar(
+        x="channel_names",
+        top="readings", width=0.9,
+        alpha=0.5,
         source=source,
     )
     layout = column(children=[channel_figure], sizing_mode='stretch_both')
