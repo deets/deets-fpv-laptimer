8442192d4147a6ac669715806a17753b5b4087c9
Diez B. Roggisch <deets@web.de>
2018-12-08 18:21:49 +0100

    Add aomway channels and improve output

--- frequency-table-generator.py
+++ frequency-table-generator.py
@@ -8,6 +8,63 @@ CHANNELS = {
     "R": [5658, 5695, 5732, 5769, 5806, 5843, 5880, 5917],  # Race Band
 }
 
+# These are according to TBS/Unify
+HAM_CHANNELS = {
+    5725, 5705, 5685, 5665, 5885, 5905,
+    5880, 5658, 5695, 5732, 5880, 5917
+}
+
+FORBIDDEN_CHANNELS = {
+    5645, 5925, 5945
+}
+
+AOMWAY_CHANNELS = (
+    (5705, "A1"),
+    (5685, "A2"),
+    (5665, "A3"),
+    (5645, "A4"),
+    (5885, "A5"),
+    (5905, "A6"),
+    (5925, "A7"),
+    (5945, "A8"),
+
+    (5733, "B1"),
+    (5752, "B2"),
+    (5771, "B3"),
+    (5790, "B4"),
+    (5809, "B5"),
+    (5828, "B6"),
+    (5847, "B7"),
+    (5866, "B8"),
+
+    (5725, "C1"),
+    (5745, "C2"),
+    (5765, "C3"),
+    (5785, "C4"),
+    (5805, "C5"),
+    (5825, "C6"),
+    (5845, "C7"),
+    (5865, "C8"),
+
+    (5740, "D1"),
+    (5760, "D2"),
+    (5780, "D3"),
+    (5800, "D4"),
+    (5820, "D5"),
+    (5840, "D6"),
+    (5860, "D7"),
+    (5880, "D8"),
+
+    (5658, "E1"),
+    (5695, "E2"),
+    (5732, "E3"),
+    (5769, "E4"),
+    (5806, "E5"),
+    (5843, "E6"),
+    (5880, "E7"),
+    (5917, "E8"),
+)
+
 
 def enumerate_channels():
     return sorted(
@@ -29,7 +86,7 @@ def main():
     vs = []
     for frequency, name in enumerate_channels():
         v = calc_register(frequency)
-        print(name, frequency, hex(v), bin(v))
+        print(name, frequency, hex(v), bin(v), "forbidden" if frequency in FORBIDDEN_CHANNELS else ("ham" if frequency in HAM_CHANNELS else "allowed"))
         vs.append(v)
     print(", ".join(hex(v) for v in vs))
 
